5d4749afd4aabc15a601f38ac8bde0e430a34cc5
Mihailo Jovanovic <mihailo.jovanovich@gmail.com>
2022-03-24 21:46:38 +0100

    Services added

---
+++ epsilon-dentistapp/src/components/TheService.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="row">
+        <div class="col-10 cell">
+            {{service.name}}
+        </div>
+        <div class="col-1 cell-price">
+            {{service.price}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TheService',
+    props: {
+        service: Object
+    }
+}
+</script>
+
+<style scoped>
+    .cell {
+        background: #9fd3c7;
+        margin: 5px;
+    }
+    .cell-price {
+        background: #9fd3c7;
+        margin: 5px;
+    }
+</style>
