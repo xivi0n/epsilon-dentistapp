0534b26f2f9a0d4f648665ce58b3105e0e853e57
Mihailo Jovanovic <mihailo.jovanovich@gmail.com>
2022-03-25 00:23:31 +0100

    Reviews added, base template, need improvement

---
+++ epsilon-dentistapp/src/components/TheReview.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="row">
+        <div class="col-3 cell">
+            {{review.name}}
+        </div>
+        <div class="col-7 cell-num">
+            {{review.info}}
+        </div>
+        <div class="col-1 cell-num">
+            {{review.num}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TheReview',
+    props: {
+        review: Object
+    }
+}
+</script>
+
+<style scoped>
+    .cell {
+        background: #9fd3c7;
+        margin: 5px;
+    }
+    .cell-num {
+        background: #9fd3c7;
+        margin: 5px;
+    }
+</style>
