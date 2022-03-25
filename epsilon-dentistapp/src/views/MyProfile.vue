4ab240dbf6c61db6e4085209e9648111461d01fd
aleksavladic <102185086+aleksavladic@users.noreply.github.com>
2022-03-25 23:18:10 +0100

    MyProfile added

--- epsilon-dentistapp/src/views/MyProfile.vue
+++ epsilon-dentistapp/src/views/MyProfile.vue
@@ -1,25 +1,45 @@
 <template>
     <TheHeader/>
-    <div class="registration container">
-        <img alt="Vue logo" src="../assets/logo.png">
-        <HelloWorld msg="MyProfile"/>
+    <div class="container bright">
+        <h2>Moj profil</h2>
+        <img alt="slika pacijenta" style="width: 20%;max-height: 100%; margin:5px" src="../assets/pacijent-za-profil.jpg">
+        <ul class="">
+                    <li class="list-group-item d-flex justify-content-between align-items-center bright-green">
+                        Ime
+                        <span style="color:#ececec">Ivana</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center bright-green">
+                        Prezime
+                        <span style="color:#ececec">Ivanic</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center bright-green">
+                        Maticni broj
+                        <span style="color:#ececec">12345678912345</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between  bright-green">
+                        Informacije
+                        <span style="color:#ececec">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum est nibh, condimentum ac pulvinar ac, ullamcorper id tortor. Nulla a sodales nulla. Morbi in iaculis lorem, non ultrices tellus. Fusce.</span>
+                    </li>        
+            </ul>
+          <button type="submit" class="btn" style="background:#FDFD96; color:darkgrey;">Prikazi prethodne izvestaje</button>  
+          <button type="submit" class="btn">Izmeni profil</button>  
     </div>
 </template>
-
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 import TheHeader from '@/components/TheHeader.vue'
 
 export default {
-  name: 'MyProfile',
+  name: 'AboutView',
   components: {
-    HelloWorld,
     TheHeader
   }
 }
 </script>
 
-<style scoped>
-    
+<style>
+.white-text {
+    display: flex;
+    justify-content: space-around;
+}
 </style>
