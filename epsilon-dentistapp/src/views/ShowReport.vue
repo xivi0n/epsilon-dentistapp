4e556b0764dc1f4abe3a6d000af9c0eb3e6f20d3
Mihailo Jovanovic <mihailo.jovanovich@gmail.com>
2022-03-25 01:05:59 +0100

    Showing all reports added

--- epsilon-dentistapp/src/views/ShowReport.vue
+++ epsilon-dentistapp/src/views/ShowReport.vue
@@ -1,22 +1,54 @@
 <template>
     <TheHeader/>
-    <div class="registration container">
-        <img alt="Vue logo" src="../assets/logo.png">
-        <HelloWorld msg="ShowReport"/>
+    <div class="container" align="center">
+        <label>Maticni broj</label>
+        <input type="number" class="form-control form-control-md"/>
+        <div :key="report.id" v-for="report in reports">
+            <TheReport :report="report"/>
+        </div>
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 import TheHeader from '@/components/TheHeader.vue'
+import TheReport from '@/components/TheReport.vue'
 
 export default {
-  name: 'ShowReport',
-  components: {
-    HelloWorld,
-    TheHeader
-  }
+    name: 'ShowReport',
+    components: {
+        TheHeader,
+        TheReport
+    },
+    data() {
+        return {
+            reports: []
+        }
+    },
+    created() {
+        this.reports = [
+            {
+                id: 1,
+                therapy: "Terapija 1",
+                diagnosis: "Dijagnoza 1"
+            },
+            {
+                id: 2,
+                therapy: "Terapija 2",
+                diagnosis: "Dijagnoza 2"
+            },
+            {
+                id: 3,
+                therapy: "Terapija 3",
+                diagnosis: "Dijagnoza 3"
+            },
+            {
+                id: 4,
+                therapy: "Terapija 4",
+                diagnosis: "Dijagnoza 4"
+            }
+        ]
+    }
 }
 </script>
 
